6100ef1259fd8ff33730d3e62d93da81b876dccb
rajanayakdevops <rajanayak.devops@gmail.com>
2025-01-19 10:44:06 +0530

    updated the server.js in branch1

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const http = require('http');
 
 const server =  http.createServer((req,res)=>{
-  console.log(" server started successfully ");
+  console.log(" on branch 1  ");
 }).listen(8000,()=>{
   console.log(" server started at port 8000");
 });
