674d7ab4b14cf4b667e92cfc65e20692299a2e71
rajanayakdevops <rajanayak.devops@gmail.com>
2025-01-23 12:26:19 +0530

    linked the user sign in and dashboard using mongo

--- controller/userController.js
+++ controller/userController.js
@@ -166,10 +166,46 @@ exports.handleRiskFormSubmission = async (req, res) => {
   }
 };
 
-exports.updateUserDashboard = (req,res) =>{
+exports.updateUserDashboard = async (req, res) => {
   const { riskCategory } = req.query;
-  return res.render("highrisk");
 
-}
+  if (!riskCategory) {
+    return res.status(400).json({ error: 'Risk category is required' });
+  }
+
+  try {
+   
+    const userId = req.session.userId; 
+
+    // Fetch the full user data from the database
+    const user = await User.findById(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+    
+    const riskCategoryNumber = parseInt(riskCategory, 10);
+    console.log(riskCategoryNumber);
+    // Check if the riskCategory matches the user's risk category in the database
+    if (user.riskCategory !== riskCategoryNumber) {
+      return res.status(400).json({ error: 'Invalid risk category for the user' });
+    }
+
+    // Render the appropriate dashboard based on the riskCategory
+    switch (riskCategoryNumber) {
+      case 1:
+        return res.render('lowrisk', { user });
+      case 2:
+        return res.render('mediumRisk', { user });
+      case 3:
+        return res.render('highrisk', { user });
+      default:
+        return res.status(400).json({ error: 'Invalid risk category' });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+};
 
 
